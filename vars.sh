0bd392e3721324921fb3a8743488865646d0b401
rafeekshaik <rafeekshaik365@gmail.com>
2025-06-05 20:42:43 +0530

    ll

--- vars.sh
+++ vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PERSON1=VENKY
 PERSON2=SURESH
-echo "$PERSON1:you $PERSON2
+echo "$PERSON1:you $PERSON2"
 echo "$PERSON2:ne rafeek. what about you"
 echo "$PERSON1:ine rahim. whare are you"
