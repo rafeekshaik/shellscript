5e48104319afe54c21bdc575f3fa3e1a0a745a28
rafeekshaik <rafeekshaik365@gmail.com>
2025-06-06 13:31:57 +0530

    ll

---
+++ installingmysql.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id-u)
+
+if [ $USERID -ne 0]
+then
+echo "error::user must have previllaged admin access"
+exit 1
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+dnf install mysql -y
+if [ $? -ne 0 ]
+then 
+echo "instaling mysql is........ failure"
+exit 1
+fi
+else
+echo "installing mysql .....success"
+fi
+else 
+echo "mysql is allready installed"
+fi
+
+    dnf list installed git
+
+    if [ $? -ne 0 ]
+    then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+    echo "installing git is a failure"
+    exit 1
+    fi
+    else
+    echo "installing git is success"
+    fi
+    else
+    echo "git is allready installed"
+    fi
