a9150a3349c1d359b1e03307b593f59607678d1f
rafeekshaik <rafeekshaik365@gmail.com>
2025-06-06 21:44:07 +0530

    kkk

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,6 +4,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 VALIDATE(){
       if [ $1 -ne 0 ]
     then 
@@ -19,21 +24,21 @@ echo "ERROR:user must have previllaged admin access"
 exit 1
 fi
 
-dnf list installed mysql
+dnf list installed mysql &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then 
-    dnf install mysql -y
+    dnf install mysql -y &>>$LOG_FILE_NAME
    VALIDATE $? "INSTALLING MYSQL"
 else
 echo "mysql is allredy installed"
 fi
 
 
-dnf list installed git
+dnf list installed git &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
-    dnf install git -y
+    dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "INSTALLING GIT"
 else
 echo -e "git allready $Y installed  $N"
