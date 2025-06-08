9fdbb53eab99ab0f83da57191588543af3ec2ae2
rafeekshaik <rafeekshaik365@gmail.com>
2025-06-08 22:53:03 +0530

    ll

--- backend.sh
+++ backend.sh
@@ -20,50 +20,50 @@ VALIDATE(){
 }
 echo "script executed at :$TIMESTAMP" &>>$LOG_FILE_NAME
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "disabling node js 16"
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
 VALIDATE $? "enabling node js 20"
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "installing nodejs"
 id expense
 if [ $? -ne 0 ]
-useradd expense
+useradd expense &>>$LOG_FILE_NAME
 VALIDATE $? "adding expense user"
 else
 echo "expense user allready created"
 fi
 
-mkdir -p /app
+mkdir -p /app &>>$LOG_FILE_NAME
 VALIDATE $? "creating app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE $? "downloading appcode"
 
-rm -rf /app/*
+rm -rf /app/* 
 cd /app
 unzip /tmp/backend.zip
 VALIDATE $? "unzipping the code"
 
-npm install
+npm install &>>$LOG_FILE_NAME
 echo "downloading dependencies"
 
-cp /home/ec2-user/shellscript/backend.sh /etc/systemd/system/backend.service
+cp /home/ec2-user/shellscript/backend.sh /etc/systemd/system/backend.service &>>$LOG_FILE_NAME
 VALIDATE $? "copying backend.service"
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "installing mysql client"
 
-mysql -h mysql.daws17s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.daws17s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
 VALIDATE $? "adding schemas to the databases"
 
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOG_FILE_NAME
 VALIDATE $? "demon relaod"
 
-systemctl start backend
+systemctl start backend &>>$LOG_FILE_NAME
 VALIDATE $? "start backend"
 
-systemctl enable backend
+systemctl enable backend &>>$LOG_FILE_NAME
 VALIDATE $? "enable baclend"
 
 
