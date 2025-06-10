a01c7f12d17134e41b3c21625088c8f229ce984e
rafeekshaik <rafeekshaik365@gmail.com>
2025-06-10 21:35:14 +0530

    kk

---
+++ backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+LOG_FOLDER="/var/log/expense-log"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+VALIDATE(){
+      if [ $1 -ne 0 ]
+    then 
+    echo -e "$2 ....... is $R failure $N"
+    exit 1
+    else
+    echo -e "$2 ....... is  $G success $N"
+    fi
+}
+USAGE (){
+echo -e "$R USAGE:: $N sh backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>"
+exit 1
+
+}
+
+
+if [ $# -lt 2]
+then
+USAGE
+fi
