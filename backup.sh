e1f68e9d283273cbff684d58071da2917da5eac2
rafeekshaik <rafeekshaik365@gmail.com>
2025-06-11 00:40:43 +0530

    kk

--- backup.sh
+++ backup.sh
@@ -9,7 +9,7 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14}
 
-LOG_FOLDER="/home/ec2-user/shellscript"
+LOG_FOLDER="/home/ec2-user/shellscript-logs"
 LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
